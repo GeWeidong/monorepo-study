7a58b7f929aab13bb806cdceb1f28488d1c79565
geweidong <geweidong@sogou-inc.com>
2021-07-23 17:12:10 +0800

    up

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,7 +2,7 @@ import { variableTypeDetection } from './is';
 
 export const init = (num: number) => {
     if (!variableTypeDetection.isNumber(num)) {
-        console.log('必须传入number类型');
+        console.log('必须传入number类型dddd');
     }
     alert(num);
 }
